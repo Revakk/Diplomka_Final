2481a02d05f57b80b4d067b6b0ef47c67a78e617
Revakhoron <Sevcik.jan42@seznam.cz>
2021-03-28 20:26:50 +0200

    Added Attacker class

--- Diplomka_SEV0123/Attacker/Attacker.h
+++ Diplomka_SEV0123/Attacker/Attacker.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "stdafx.h"
+
+
+
+class Attacker
+{
+public:
+	void set_target(std::filesystem::path p);
+	void execute_payload();
+	void execute_exe();
+private:
+	std::filesystem::path target_file;
+};
