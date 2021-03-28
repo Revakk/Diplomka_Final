fb6f33c075575868de6e96b265e35e90858da6b7
Revakhoron <Sevcik.jan42@seznam.cz>
2021-03-28 20:14:50 +0200

    class Infector completed? maybe some changes later on

--- Diplomka_SEV0123/Infector/Infector.cpp
+++ Diplomka_SEV0123/Infector/Infector.cpp
@@ -1,6 +1,81 @@
 #include "Infector.h"
 
 
+bool Infector::file_exists(std::filesystem::path p)
+{
+    return (std::filesystem::exists(p)) ? true : false;
+}
+
+
+void Infector::read_file(std::filesystem::path p)
+{
+    if (!file_exists(p))
+    {
+        std::cout << "Cannot read file that does not exists!";
+    }
+
+    std::ifstream inf_file(p);
+    std::string file_path = "";
+    std::string delimiter = ",";
+    int index_file = 0;
+    std::string line = "";
+    size_t pos = 0;
+
+
+    if (inf_file.is_open())
+    {
+        while (std::getline(inf_file, line))
+        {
+            file_path = line.substr(0, line.find(delimiter));
+            index_file = std::stoi(line.erase(0, line.find(delimiter) + delimiter.length()));
+            infected_files.emplace_back(std::move(std::make_pair(file_path, index_file)));
+        }
+    }
+
+
+    inf_file.close();
+
+
+}
+
+void Infector::write_file(std::filesystem::path p)
+{
+    std::fstream inf_file(p);
+    if(!(inf_file.peek() == std::ifstream::traits_type::eof()))
+    {
+        inf_file.clear();
+    }
+
+
+    for (auto& f : infected_files)
+    {
+        inf_file << f.first << ',' << f.second << '\n';
+    }
+
+
+
+    inf_file.close();
+}
+
+
+std::filesystem::path Infector::get_path_by_index(int index)
+{
+    if (infected_files.empty())
+    {
+        read_file(path_to_file);
+    }
+
+    for (auto& f : infected_files)
+    {
+        if (f.second == index)
+        {
+            return (f.first);
+            break;
+        }
+    }
+
+}
+
 std::string Infector::get_exe_name(const std::filesystem::path& p) const
 {
     std::string exe_path = p.u8string();
--- Diplomka_SEV0123/Infector/Infector.h
+++ Diplomka_SEV0123/Infector/Infector.h
@@ -11,11 +11,14 @@ public:
 	bool file_already_running(std::filesystem::path p);
 private:
 	bool file_exists(std::filesystem::path p);
-	void create_file(std::filesystem::path p);
+	void read_file(std::filesystem::path p);
+	void write_file(std::filesystem::path p);
 	void get_exes(std::vector<std::filesystem::path>& dir_paths, std::vector<std::pair<std::filesystem::path, int>>& exe_paths);
 	std::string get_exe_name(const std::filesystem::path& p) const;
 
+
+	std::filesystem::path path_to_file = "infected_files.txt";
 	int number_of_files = 0;
 	std::vector<std::pair<std::filesystem::path, int>> infected_files;
-
+	std::filesystem::path file_path = "infected_files.txt";
 };
